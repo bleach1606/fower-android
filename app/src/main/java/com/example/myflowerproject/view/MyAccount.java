76817c79e60cdec648e358a547c4410fad8743d7
bleach1606 <bl.bleach1606@gmail.com>
2020-05-25 01:38:13 +0700

    update fordel code

--- app/src/main/java/com/example/myflowerproject/view/MyAccount.java
+++ app/src/main/java/com/example/myflowerproject/view/MyAccount.java
@@ -1,8 +1,10 @@
-package com.example.myflowerproject;
+package com.example.myflowerproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.myflowerproject.R;
 
 public class MyAccount extends AppCompatActivity {
 
