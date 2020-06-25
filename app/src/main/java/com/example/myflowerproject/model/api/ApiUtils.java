f122fa299b939e78568b3db946b95ef3bc90d629
nguyen khac thanh <bl.bleach1606@gmail.com>
2020-06-25 15:41:03 +0700

    update hehe

--- app/src/main/java/com/example/myflowerproject/model/api/ApiUtils.java
+++ app/src/main/java/com/example/myflowerproject/model/api/ApiUtils.java
@@ -3,7 +3,7 @@ package com.example.myflowerproject.model.api;
 public class ApiUtils {
     private ApiUtils() {}
 
-    public static final String BASE_URL = "http://192.168.43.32:8080/";
+    public static final String BASE_URL = "http://192.168.43.209:8080/";
 
     public static UserAPI getAPIService() {
         return RetrofitClient.getClient(BASE_URL).create(UserAPI.class);
